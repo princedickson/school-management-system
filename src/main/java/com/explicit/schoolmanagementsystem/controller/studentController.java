dcb1b37bfac35ca4e5944cf85d2db9028ae01d35
explicit <princedickson03@gmail.com>
2023-01-19 00:46:32 +0100

    new

--- src/main/java/com/explicit/schoolmanagementsystem/controller/studentController.java
+++ src/main/java/com/explicit/schoolmanagementsystem/controller/studentController.java
@@ -3,11 +3,13 @@ package com.explicit.schoolmanagementsystem.controller;
 import com.explicit.schoolmanagementsystem.entity.student;
 import com.explicit.schoolmanagementsystem.service.studentService;
 import jakarta.persistence.ManyToOne;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class studentController {
@@ -20,19 +22,19 @@ public class studentController {
     //handle list of student and return model and view
     @GetMapping("/student")
     public String listStudent(Model model){
-        model.addAttribute ( "student", studentServicee.getAllStudent () );
+        model.addAttribute ( "students", studentServicee.getAllStudent () );
         return "students";
 
     }
     @GetMapping("/student/new")
     public  String createStudentForm(Model model){
         student Student= new student ();
-        model.addAttribute ( "Student",Student );
+        model.addAttribute ( "students",Student );
         return "create_student";
 
     }
-    @PostMapping()
-    public  String saveStudent(@ModelAttribute ("Student") student Student){
+    @PostMapping("/student")
+    public  String saveStudent(@ModelAttribute ("student") student Student){
        studentServicee.saveStudent ( Student );
        return "redirect:/student";
 
